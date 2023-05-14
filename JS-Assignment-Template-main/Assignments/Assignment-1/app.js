62c6e3a03922141002b6dcef8d3b68e900323f41
falaknazsaeed <falaknazseed2@gmail.com>
2023-05-14 09:47:57 +0500

    Assignments

---
+++ JS-Assignment-Template-main/Assignments/Assignment-1/app.js
@@ -0,0 +1,21 @@
+//alert("hello")
+
+//ASSIGNMENT-#-01-//
+
+//Task-01. Write a script to greet your website visitor using JS alert box.
+
+alert("Hello User! Welcome to my website");
+
+
+
+
+// Task-02. Write a script to display following message on your web page:
+
+alert("Error! Please enter a valid password.");
+
+
+
+
+// Task-03. Write a script to display following message on your web page: (Hint : Use line break)
+
+alert("Welcome to JS Land...\nHappy Coding!");
